483ee98d3def9ce0ab41a9319db3bfd125140653
Uri Miller <usm3361@gmail.com>
2026-02-26 17:16:44 +0200

    refactor: clean up product and cart service code for consistency and readability

--- Backend/src/models/productModel.js
+++ Backend/src/models/productModel.js
@@ -1,16 +1,13 @@
 import { getMongoConnection } from "../config/dbConn.js";
-import { ObjectId } from 'mongodb'
+import { ObjectId } from "mongodb";
 
-const db = await getMongoConnection()
+const db = await getMongoConnection();
 const productCollection = db.collection("products");
 
-export async function getAllProductsModel() { 
-    const productsArr = await productCollection.find().toArray()
-    return productsArr
+export async function getAllProductsModel() {
+  return await productCollection.find().toArray();
 }
 
 export async function getProductByIdModel(id) {
-const product = await productCollection.findOne({_id:new ObjectId(id)});
-return product;
+  return await productCollection.findOne({ _id: new ObjectId(id) });
 }
-
--- Backend/src/services/productService.js
+++ Backend/src/services/productService.js
@@ -1,13 +1,12 @@
-import { getAllProductsModel, getProductByIdModel } from "../models/productModel.js";
+import {
+  getAllProductsModel,
+  getProductByIdModel,
+} from "../models/productModel.js";
 
 export async function getAllProductsService() {
-    const productsArr = await getAllProductsModel()
-    return productsArr
+  return await getAllProductsModel();
 }
 
-export async function getProductByIdService(id) { 
-    const product = await getProductByIdModel(id)
-    return product
+export async function getProductByIdService(id) {
+  return await getProductByIdModel(id);
 }
-
-
